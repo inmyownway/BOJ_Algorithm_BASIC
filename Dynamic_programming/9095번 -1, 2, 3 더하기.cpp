a6fcd5ee6ff1dfbfd7c6ad803b1b38dbf74a5cb1
wjdals0507@naver.com <wjdals0507@naver.com>
2021-10-06 23:46:52 +0900

    BOJ

---
+++ Dynamic_programming/9095번 -1, 2, 3 더하기.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+
+using namespace std;
+int d[11];
+
+
+int main()
+{
+    d[0] = 1;
+
+    for (int i = 1; i <= 10; i++) {
+        if (i - 1 >= 0) {
+            d[i] += d[i - 1];
+        }
+        if (i - 2 >= 0) {
+            d[i] += d[i - 2];
+        }
+        if (i - 3 >= 0) {
+            d[i] += d[i - 3];
+        }
+    }
+ 
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        cout << d[n] << endl;
+
+    }
+
+
+}
