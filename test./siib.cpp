7f1d04651cc14534e96b463b9c71eecd57b077ba
jungmin <wjdals05070@gmail.com>
2022-01-25 17:07:35 +0900

    it me

---
+++ test./siib.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout<<"test";
+    std::cout<<"test";
+    
+//adsdsd
+    std::cout<<"test";
+    std::cout<<"test";
+    std::cout<<"test";
+    std::cout<<"test";
+    std::cout<<"test";
+    std::cout<<"test";
+    
+    return 0;
+}
