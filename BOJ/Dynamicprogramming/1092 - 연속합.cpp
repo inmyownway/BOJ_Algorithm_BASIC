59405a587f5caba6889719089f555775aaf4cf7b
jungmin <wjdals05070@gmail.com>
2022-01-30 21:30:05 +0900

    bo

---
+++ BOJ/Dynamicprogramming/1092 - 연속합.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    
+    int n;
+    cin>>n;
+    vector<int> d(n);
+    for(int i=0;i<n;i++)
+    {
+       int in;
+       cin>>in;
+       v.push_back(in);
+    }
+    
+    for(int i=0;i<n;i++)
+    {   
+        
+        d[i]=v[i];
+        if(i==0)
+            continue;
+        if(d[i-1]+v[i]>d[i])
+            d[i]=d[i-1]+v[i];
+            
+    }
+    
+    int max = *max_element(d.begin(), d.end());
+    cout<<max;
+    return 0;
+
+}
